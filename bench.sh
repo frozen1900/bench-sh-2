0694caf19c7ef6e3ffe3cc47e45ed774814cdb80
Hidden Refuge <hidden-refuge@users.noreply.github.com>
2018-05-09 07:21:15 +0200

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -86,6 +86,8 @@ speedtest4 () {
 	echo "Tokyo, Japan		Linode		$linodejp " | tee -a $HOME/bench.log
 	slsg=$( wget -4 -O /dev/null http://speedtest.sng01.softlayer.com/downloads/test100.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
 	echo "Singapore 		Softlayer	$slsg " | tee -a $HOME/bench.log
+	hitw=$( wget -4 -O /dev/null http://tpdb.speed2.hinet.net/test_100m.zip 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' ) 
+	echo "Taiwan                    Hinet           $hitw " | tee -a $HOME/bench.log
 	echo "" | tee -a $HOME/bench.log
 	# Europe speed test
 	i3d=$( wget -4 -O /dev/null http://mirror.i3d.net/100mb.bin 2>&1 | awk '/\/dev\/null/ {speed=$3 $4} END {gsub(/\(|\)/,"",speed); print speed}' )
